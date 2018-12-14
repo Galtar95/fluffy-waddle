ae75d7c1617d6f7c27f4f54c564a556ce7ccf6fa
Jean-François Ngo <ngo_j@epita.fr>
2018-12-14 20:15:20 +0100

    fefedzdz

--- src/Game/PlayerIA/Galtar95Player.php
+++ src/Game/PlayerIA/Galtar95Player.php
@@ -41,41 +41,25 @@ class Galtar95Player extends Player
         // How can i display the result of each round ? $this->prettyDisplay()
         // -------------------------------------    -----------------------------------------------------
 
-        if ($this->result->getNbRound() == 0) {
+        if ($this->result->getNbRound() == 0)
             return 'foe';
-        }
+        if (!in_array('foe',  $this->result->getChoicesFor($this->opponentSide)))
+            return 'foe';
+        if (!in_array('friend',  $this->result->getChoicesFor($this->opponentSide)))
+            return 'foe';
         $friend = 0;
         $foe = 0;
-        $all = $this->result->getChoicesFor($this->mySide);
-        for ($i = 0; $i < $this->result->getNbRound(); $i++) {
+        $all = $this->result->getChoicesFor($this->opponentSide);
+        for ($i = 0; $i < count($all); $i++) {
             if ($all[$i] == 'friend')
                 $friend++;
             else
                 $foe++;
         }
         if ($friend >= $foe)
-            return 'friend';
+            return 'foe';
         else
-            return 'foe';
-        if (!in_array('foe',  $this->result->getChoicesFor($this->opponentSide))) {
-            return 'foe';
-        }
-        if (!in_array('friend',  $this->result->getChoicesFor($this->opponentSide))) {
             return 'friend';
-        }
-        if (!in_array('foe',  $this->result->getChoicesFor($this->mySide))) {
-            return 'foe';
-        }
-        if (!in_array('friend',  $this->result->getChoicesFor($this->mySide))) {
-            return 'friend';
-        }
-
-        if ($this->result->getLastChoiceFor($this->opponentSide) == 'foe') {
-            return 'friend';
-        }
-        if ($this->result->getLastChoiceFor($this->opponentSide) == 'friend') {
-            return 'foe';
-        }
     }
  
 };
