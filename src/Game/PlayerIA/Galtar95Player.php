49f999b713b9e8c788db7deb379f742a7c1fda1a
Jean-François Ngo <ngo_j@epita.fr>
2018-12-14 19:20:47 +0100

    fefe

--- src/Game/PlayerIA/Galtar95Player.php
+++ src/Game/PlayerIA/Galtar95Player.php
@@ -41,18 +41,20 @@ class Galtar95Player extends Player
         // How can i display the result of each round ? $this->prettyDisplay()
         // -------------------------------------    -----------------------------------------------------
 
-        if (!$this->result->getLastChoiceFor($this->opponentSide))
+        if ($this->result->getLastChoiceFor($this->mySide) == 0)
             return 'foe';
-        if (!in_array('foe', $this->result->getChoicesFor($this->opponentSide)))
+        if (!in_array('foe', $this->result->getStatsFor($this->opponentSide)))
             return 'friend';
-        if (!in_array('friend', $this->result->getChoicesFor($this->opponentSide)))
+        if (!in_array('friend', $this->result->getStatsFor($this->opponentSide)))
             return 'foe';
-        if (!in_array('foe', $this->result->getChoicesFor($this->mySide)))
+        if (!in_array('foe', $this->result->getStatsFor($this->mySide)))
             return 'friend';
-        if (!in_array('friend', $this->result->getChoicesFor($this->mySide)))
+        if (!in_array('friend', $this->result->getStatsFor($this->mySide)))
             return 'foe';
-        else
-            return $this->result->getLastChoiceFor($this->opponentSide);
+        if ($this->result->getLastChoiceFor($this->opponentSide) == 'foe')
+            return 'friend';
+        if ($this->result->getLastChoiceFor($this->opponentSide) == 'friend')
+            return 'foe';
     }
  
 };
